37886c4a34350560590646bebd3b2bad1b495594
Cohey0727 <ohayousagi.ac.kook0727@gmail.com>
2020-02-16 18:50:54 +0900

    master

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js changed!!</code> and save to reload.
         </p>
+        <p>This is Master Branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
